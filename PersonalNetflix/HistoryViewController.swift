4b875210d3a17d3ca36289c8488169d04debf3e0
김동환 <1033513@naver.com>
2021-06-07 19:11:56 +0900

    firebase realtime database에서 사용자가 검색한 목록 순서대로 불러오기

--- PersonalNetflix/HistoryViewController.swift
+++ PersonalNetflix/HistoryViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 class HistoryViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var histories: [SearchHistory] = []
     let db = Database.database().reference().child("searchHistory")
     
     override func viewDidLoad() {
@@ -22,17 +23,58 @@ class HistoryViewController: UIViewController {
         super.viewWillAppear(animated)
         
         db.observeSingleEvent(of: .value) { snapshot in
-            print("검색 목록 \(snapshot.value)")
-        }
         
+            guard let value = snapshot.value as? [String : Any] else {return}
+            
+          
+            guard let data = try? JSONSerialization.data(withJSONObject: Array(value.values), options: []) else {return}
+            
+            do{
+                let histories = try JSONDecoder().decode([SearchHistory].self, from: data)
+                
+                let sortedHistory = histories.sorted { term1, term2 in
+                    term1.timestamp > term2.timestamp
+                }
+                self.histories = sortedHistory
+                
+                self.tableView.reloadData()
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension HistoryViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        print(self.histories.count)
+        return self.histories.count
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell") as? HistoryCell else {return UITableViewCell()}
+        
+        let movie = self.histories[indexPath.row]
+        
+        cell.searchLabel.text = movie.term
+        return cell
+    }
 }
+
+
 
 class HistoryCell: UITableViewCell {
     
     @IBOutlet weak var searchLabel: UILabel!
     
+}
+
+struct SearchHistory: Codable {
+    
+    let term: String
+    let timestamp: TimeInterval
     
 }
